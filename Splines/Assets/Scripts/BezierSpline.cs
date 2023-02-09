489d3a251afd5f09a8f348c0de0dd1f2f7073a22
Sofi <w0lfskitten173@gmail.com>
2023-02-09 15:27:30 +1100

    working bezier spline

--- Splines/Assets/Scripts/BezierSpline.cs
+++ Splines/Assets/Scripts/BezierSpline.cs
@@ -7,66 +7,6 @@ using System;
 
 namespace Splines
 {
-    // To move and visualise the anchor points
-    [Serializable]
-    public class AnchorPoints
-    {
-        public List<Vector3> anchorPoints = new List<Vector3>();
-    }
-
-    public class BezierCurve
-    {
-        // First 4 are control points, will have to change to 3 and an end point
-        // Last 5 are lerping points
-        private List<Vector3> points;
-        private AnchorPoints anchorPoints = new AnchorPoints();
-
-        // creates the bezier curve
-        public void Initialise(int a_points)
-        {
-            points = new List<Vector3>();
-            for (int i = 0; i < a_points; i++)
-            {
-                points.Add(Vector3.zero);
-            }
-
-            for (int i = 0; i < 4; i++)
-            {
-                anchorPoints.anchorPoints.Add(points[i]);
-            }
-        }
-
-        // get all the points of a bezier curve
-        public List<Vector3> GetPoints()
-        {
-            return points;
-        }
-
-        // set all the points of a bezier curve
-        public void SetPoints(List<Vector3> a_points)
-        {
-            points = a_points;
-        }
-
-        // get a specific point
-        public Vector3 GetPoint(int a_index)
-        {
-            return points[a_index];
-        }
-
-        // set a specific point
-        public void SetPoint(int a_index, Vector3 a_value)
-        {
-            points[a_index] = a_value;
-        }
-
-        // get anchor points
-        public AnchorPoints GetAnchorPoints()
-        {
-            return anchorPoints;
-        }
-    }
-
     [ExecuteInEditMode]
     public class BezierSpline : MonoBehaviour
     {
@@ -82,22 +22,20 @@ namespace Splines
         [SerializeField] private int numOfCurves = 0;
         // number of bezier curves on last frame
         private int numOfCurvesLastFrame = 0;
-        [SerializeField] private List<AnchorPoints> anchorPoints = new List<AnchorPoints>();
 
         // tracker point control value
-        [SerializeField] [Range(0f, 1f)] private float tValue = 0f;
+        [SerializeField] [Range(0f, 3f)] private float tValue = 0f;
 
         // the point that moves
-        private Vector3 trackingPoint = Vector3.zero;
+        [SerializeField] private Vector3 trackingPoint = Vector3.zero;
 
-        // static number of how many points are in a curve
-        static private int numPointsInCurve = 9;
         // list of the all the bezier curves
-        private List<BezierCurve> curves = new List<BezierCurve>();
+        [SerializeField] private List<Vector3> points = new List<Vector3>();
+        private Vector3 endPoint = Vector3.zero;
         
         void Start()
         {
-
+            points.Add(endPoint);
         }
 
         void Update()
@@ -107,9 +45,7 @@ namespace Splines
             if (numOfCurvesLastFrame > numOfCurves)
                 RemoveCurve();
 
-            UpdatePoints();
-
-            if (curves.Count != 0 && anchorPoints.Count != 0)
+            if (points.Count != 0)
                 PolynomialCoefficients();
 
             // updates for next frame
@@ -118,47 +54,53 @@ namespace Splines
 
         public void AddCurve()
         {
-            // creates new bezier curve
-            BezierCurve curve = new BezierCurve();
-            curve.Initialise(numPointsInCurve);
-            // adds new bezier curve to lists
-            curves.Add(curve);
-            anchorPoints.Add(curve.GetAnchorPoints());
+            if (points.Count != 0)
+            {
+                endPoint = points[points.Count - 1];
+                points.RemoveAt(points.Count - 1);
+            }
+
+            Vector3 startPos = endPoint;
+            for (int i = 0; i < 3; i++)
+            {
+                points.Add(startPos);
+            }
+
+            points.Add(endPoint);
         }
 
         public void RemoveCurve()
         {
-
-
-            
-                curves.Clear();
-                anchorPoints.Clear();
+            if (points.Count == 0)
+            {
+                points.Clear();
                 return;
+            }
 
-            // removes bezier curve at end of lists
-            curves.Remove(curves[curves.Count - 1]);
-            anchorPoints.Remove(anchorPoints[anchorPoints.Count - 1]);
+            endPoint = points[points.Count - 1];
+
+            for (int i = 0; i < 4; i++)
+            {
+                points.RemoveAt(points.Count - 1);
+            }
+
+            points.Add(endPoint);
         }
 
         public void UpdatePoints()
         {
-            // allows the anchor points to be moved from the inspector
-            for (int i = 0; i < curves.Count; i++)
-            {
-                for (int j = 0; j < 4; j++)
-                {
-                    curves[i].SetPoint(j, anchorPoints[i].anchorPoints[j]);
-                }
-            }
+            
         }
 
         public void Bernstein()
         {
+            int localCurve = (int)tValue;
+
             // more readable anchor points
-            Vector3 p0 = curves[0].GetPoint(0);
-            Vector3 p1 = curves[0].GetPoint(1);
-            Vector3 p2 = curves[0].GetPoint(2);
-            Vector3 p3 = curves[0].GetPoint(3);
+            Vector3 p0 = points[0 + localCurve];
+            Vector3 p1 = points[1 + localCurve];
+            Vector3 p2 = points[2 + localCurve];
+            Vector3 p3 = points[3 + localCurve];
 
             // split into 4 lines
             Vector3 con1 = p0 * (Mathf.Pow(-tValue, 3) + 3 * Mathf.Pow(tValue, 2) - 3 * tValue + 1);
@@ -171,19 +113,41 @@ namespace Splines
 
         public void PolynomialCoefficients()
         {
+            int localCurve = (int)tValue + 2 * (int)tValue;
+            float localT = tValue - (int)tValue;
+
             // more readable anchor points
-            Vector3 p0 = curves[0].GetPoint(0);
-            Vector3 p1 = curves[0].GetPoint(1);
-            Vector3 p2 = curves[0].GetPoint(2);
-            Vector3 p3 = curves[0].GetPoint(3);
+            Vector3 p0 = points[0 + localCurve];
+            Vector3 p1 = points[1 + localCurve];
+            Vector3 p2 = points[2 + localCurve];
+            Vector3 p3 = points[3 + localCurve];
 
             // split exquation into 3 separate lines
-            Vector3 con1 = tValue * (-3 * p0 + 3 * p1);
-            Vector3 con2 = tValue * tValue * (3 * p0 - 6 * p1 + 3 * p2);
-            Vector3 con3 = tValue * tValue * tValue * (-p0 + 3 * p1 - 3 * p2 + p3);
+            Vector3 con1 = localT * (-3 * p0 + 3 * p1);
+            Vector3 con2 = localT * localT * (3 * p0 - 6 * p1 + 3 * p2);
+            Vector3 con3 = localT * localT * localT * (-p0 + 3 * p1 - 3 * p2 + p3);
 
             // add all together
             trackingPoint = p0 + con1 + con2 + con3;
+        }
+
+        public Vector3 PolynomialCoefficientsReturn(float a_tValue)
+        {
+            int localCurve = (int)tValue;
+
+            // more readable anchor points
+            Vector3 p0 = points[0 + localCurve];
+            Vector3 p1 = points[1 + localCurve];
+            Vector3 p2 = points[2 + localCurve];
+            Vector3 p3 = points[3 + localCurve];
+
+            // split exquation into 3 separate lines
+            Vector3 con1 = a_tValue * (-3 * p0 + 3 * p1);
+            Vector3 con2 = a_tValue * a_tValue * (3 * p0 - 6 * p1 + 3 * p2);
+            Vector3 con3 = a_tValue * a_tValue * a_tValue * (-p0 + 3 * p1 - 3 * p2 + p3);
+
+            // add all together
+            return p0 + con1 + con2 + con3;
         }
 
         public Vector3 V3Lerp(Vector3 a, Vector3 b, float t)
@@ -193,35 +157,34 @@ namespace Splines
 
         private void OnDrawGizmos()
         {
+            if (showLines && points.Count != 0)
+            {
+                // draws the straight lines connecting the anchor points
+                Gizmos.color = Color.white;
+                for (int i = 0; i < points.Count - 3; i += 3)
+                {
+                    Gizmos.DrawLine(points[i], points[i + 1]);
+                    Gizmos.DrawLine(points[i + 2], points[i + 3]);
+                }
+            }
+
+            if (showPoints && points.Count != 0)
+            {
+                // draws the anchor points
+                Gizmos.color = Color.blue;
+                for (int i = 0; i < points.Count; i++)
+                {
+                    Gizmos.DrawWireSphere(points[i], pointSize);
+
+                    UnityEditor.Handles.Label(points[i], "P" + i.ToString());
+                }
+            }
+
             if (showTrackingPoint)
             {
                 // draws the tracking point
                 Gizmos.color = Color.yellow;
                 Gizmos.DrawWireSphere(trackingPoint, pointSize);
-            }
-
-            if (showPoints && curves.Count != 0)
-            {
-                // draws the anchor points
-                Gizmos.color = Color.blue;
-                for (int i = 0; i < curves.Count; i++)
-                {
-                    for (int j = 0; j < curves[i].GetPoints().Count; j++)
-                    {
-                        Gizmos.DrawWireSphere(curves[i].GetPoint(j), pointSize);
-                    }
-                }
-            }
-
-            if (showLines && curves.Count != 0)
-            {
-                // draws the straight lines connecting the anchor points
-                Gizmos.color = Color.white;
-                for (int i = 0; i < curves.Count; i++)
-                {
-                    Gizmos.DrawLine(curves[i].GetPoint(0), curves[i].GetPoint(1));
-                    Gizmos.DrawLine(curves[i].GetPoint(2), curves[i].GetPoint(3));
-                }
             }
         }
     }
